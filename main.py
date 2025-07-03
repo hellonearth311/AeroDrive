c12db7bcfd8adeaf4bb51d309996e443572a8cc0
hellonearth311 <narangswarit02@gmail.com>
2025-07-03 15:54:43 -0500

    Add a plane and make the camera spin around it

--- main.py
+++ main.py
@@ -20,27 +20,17 @@ class MyApp(ShowBase):
         # to make a task work, you need to add it to the task manager
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-        # an actor is a model with animations
-        # we can load it in like this
-        self.pandaActor = Actor("models/panda-model",
-                                {"walk": "models/panda-walk4"})
-        
-        # now we load it in like a normal model and make it loop the walking animation
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
-        self.pandaActor.reparentTo(self.render)
-
-        self.pandaActor.loop("walk")
-
         # let me try loading in a plane
-        self.planeModel = self.loader.loadModel("assets/models/plane.egg")
+        self.planeModel = self.loader.loadModel("assets/models/C172.glb")
         self.planeModel.reparentTo(self.render)
 
-        self.planeModel.setScale(4, 4, 4)
+        self.planeModel.setScale(1.5, 1.5, 1.5)
+        self.planeModel.setPos(0, 0, 4)
 
     def spinCameraTask(self, task):
         # tasks are functions that will be called every frame
         # this task spins the camera around using some fancy math i dont quite understand
-        angleDegrees = task.time * 6.0
+        angleDegrees = task.time * 12.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
